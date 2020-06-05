03341a962a65fd235fc5e85ae6d483ff1c4a2a81
changshu.li <lcs.005@163.com>
2020-06-05 16:32:19 +0800

    socket test

---
+++ src/test/java/com/github/microwww/RequestTest.java
@@ -0,0 +1,49 @@
+package com.github.microwww;
+
+import org.junit.Test;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class RequestTest {
+
+    // @Test
+    public void test() throws IOException, InterruptedException {
+        //while (true) {
+        Thread th = new Thread(() -> {
+            int i = 0;
+            for (; i < Integer.MAX_VALUE; i++) {
+                try {
+                    Socket sk = new Socket();
+                    sk.connect(new InetSocketAddress("localhost", 10421), 1000);
+                    BufferedOutputStream out = new BufferedOutputStream(sk.getOutputStream());
+                    InetSocketAddress cd = (InetSocketAddress) sk.getLocalSocketAddress();
+                    System.out.println("IN port :" + cd.getPort());
+                    for (; i < Integer.MAX_VALUE; i++) {
+                        out.write(("test: " + i + " [" + cd.getPort() + "] ").getBytes());
+                        out.flush();
+                        Thread.sleep(1000);
+                    }
+                    sk.close();
+                } catch (Exception e) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        Thread.interrupted();
+                    }
+                }
+            }
+        });
+        th.start();
+        th.join();
+        //Thread.sleep(50000);
+        //}
+    }
+
+    @Test
+    public void test1() {
+    }
+}
