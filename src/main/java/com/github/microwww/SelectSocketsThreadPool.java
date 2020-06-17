55231a42105ec563339102d69ad8527291075694
changshu.li <lcs.005@163.com>
2020-06-17 11:37:02 +0800

    remove task

--- src/main/java/com/github/microwww/SelectSocketsThreadPool.java
+++ src/main/java/com/github/microwww/SelectSocketsThreadPool.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 public class SelectSocketsThreadPool extends SelectSockets {
 
-    private ConcurrentHashMap<String, TaskThread> tasks = new ConcurrentHashMap();
+    private Map<String, TaskThread> tasks = new ConcurrentHashMap();
     private final Executor pool;
 
     public SelectSocketsThreadPool(Executor pool) {
@@ -38,6 +38,7 @@ public class SelectSocketsThreadPool extends SelectSockets {
                 tasks.put(k, thread); // 原先已经停止, 开启新的读取
                 thread.scheduling((lock) -> {
                     readChannel(channel, lock);
+                    tasks.remove(k, thread);
                 });
             } catch (RuntimeException | IOException e) { // IO 做简单处理
                 try {
