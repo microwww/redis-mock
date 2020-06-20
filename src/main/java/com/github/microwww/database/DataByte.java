2fdd80e5575eb081ac359c2ef0d9dbe59b799728
changshu.li <lcs.005@163.com>
2020-06-20 15:21:17 +0800

    data set

--- src/main/java/com/github/microwww/database/DataByte.java
+++ src/main/java/com/github/microwww/database/DataByte.java
@@ -2,6 +2,8 @@ package com.github.microwww.database;
 
 import com.github.microwww.util.Assert;
 
+import java.util.Comparator;
+
 public class DataByte extends AbstractValueData<byte[]> {
 
     public DataByte(byte[] data, int exp) {
@@ -9,4 +11,14 @@ public class DataByte extends AbstractValueData<byte[]> {
         this.data = data;
         this.expire = exp;
     }
+
+    public static final Comparator<byte[]> COMPARATOR = (o1, o2) -> {
+        int length = Math.min(o1.length, o2.length);
+        for (int i = 0; i < length; i++) {
+            if (o1[i] != o2[i]) {
+                return o1[i] - o2[i];
+            }
+        }
+        return o1.length - o2.length;
+    };
 }
--- src/main/java/com/github/microwww/database/DataSet.java
+++ src/main/java/com/github/microwww/database/DataSet.java
@@ -1,6 +1,51 @@
 package com.github.microwww.database;
 
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 public class DataSet extends AbstractValueData<Set<byte[]>> {
+    private final Set<byte[]> origin;
+
+    public DataSet() {
+        this.origin = new ConcurrentSkipListSet<>(DataByte.COMPARATOR);
+        this.data = Collections.unmodifiableSet(this.origin);
+    }
+
+    //SADD
+    public synchronized boolean add(byte[] bytes) {
+        return origin.add(bytes);
+    }
+
+    //SCARD
+    //SDIFF
+    //SDIFFSTORE
+    //SINTER
+    //SINTERSTORE
+    //SISMEMBER
+    //SMEMBERS
+    //SMOVE
+    public synchronized boolean remove(byte[] o) {
+        return origin.remove(o);
+    }
+
+    //SPOP
+    public synchronized Optional<byte[]> pop() {
+        Optional<byte[]> bytes = this.randMember();
+        bytes.ifPresent(this.origin::remove);
+        return bytes;
+    }
+
+    //SRANDMEMBER
+    public synchronized Optional<byte[]> randMember() {
+        Iterator<byte[]> it = origin.iterator();
+        if (it.hasNext()) {
+            byte[] e = it.next();
+            return Optional.of(e);
+        }
+        return Optional.empty();
+    }
+    //SREM
+    //SUNION
+    //SUNIONSTORE
+    //SSCAN
 }
