3518aca5b0190a5f0a8e3190d7dc910cae81620d
changshu.li <lcs.005@163.com>
2021-06-01 10:37:47 +0800

    multiple test bind error

--- mocker-redis-spring-boot-starter/src/main/java/com/github/microwww/spring/boot/redis/mock/RedisServerListener.java
+++ mocker-redis-spring-boot-starter/src/main/java/com/github/microwww/spring/boot/redis/mock/RedisServerListener.java
@@ -9,17 +9,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
 @ConditionalOnProperty(prefix = "mocker.redis", value = "enable", havingValue = "true", matchIfMissing = true)
 public class RedisServerListener {
 
     private static final Logger logger = LoggerFactory.getLogger(RedisServerListener.class);
+    private static RedisServer server; // 运行多个单元测试的时候, 会启动多个 ApplicationContext, 导致多次端口绑定, 所以此处缓存
+    private static AtomicInteger count = new AtomicInteger(0);
 
     @Bean(destroyMethod = "close")
     public RedisServer mockRedisServer(RedisProperties redisProperties) throws IOException {
-        RedisServer server = new RedisServer();
-        server.listener(redisProperties.getHost(), redisProperties.getPort());
+        if (server == null) synchronized (RedisServerListener.class) {
+            if (server == null) {
+                RedisServer server = new RedisServer() {
+                    @Override
+                    public void close() throws IOException {
+                        if (count.decrementAndGet() <= 0) {
+                            logger.info("Mocker Redis will STOP !");
+                            super.close();
+                        }
+                    }
+                };
+                server.listener(redisProperties.getHost(), redisProperties.getPort());
+                this.server = server;
+            }
+        }
+        count.incrementAndGet();
         logger.info("Mocker Redis start :: [{}:{}], set 'server.redis.host' to match it", redisProperties.getHost(), redisProperties.getPort());
         return server;
     }
